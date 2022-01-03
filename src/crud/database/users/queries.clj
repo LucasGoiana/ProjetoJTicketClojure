8b5ee987488c53d30b4fb23c0d78a03f8e14b444
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2022-01-03 20:57:14 -0300

    adicionando rotas de usuarios

--- src/crud/database/users/queries.clj
+++ src/crud/database/users/queries.clj
@@ -1,9 +1,16 @@
 (ns crud.database.users.queries
   (:use
-    [crud.database.connection]),
+    [crud.database.connection],
+    [next.jdbc.date-time]),
   (:require
     [next.jdbc.sql :as query]))
 
-
+; inserir usuario
 (defn inserir [id-profile name email password slug] (query/insert! ds :usuario
-              {:idPerfil id-profile :Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.) }) )
+              {:idPerfil id-profile :Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.)}))
+
+;atualizar usuario pelo id
+(defn atualizar [id name email password slug] (query/update! ds :usuario
+              {:Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.)}    {:idUsuario id } ))
+
+(defn ler [request] (read-as-local) (query/query ds ["select * from usuario"]))
