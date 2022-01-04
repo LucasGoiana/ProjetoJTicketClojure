2c4a0a5a8a808be5658dd3d6d3e1a307e72d4b76
Lucas Goiana Malicia <lucasgoianam@hotmail.com>
2022-01-03 21:52:22 -0300

    finalizando rotas de usuarios

--- src/crud/database/users/queries.clj
+++ src/crud/database/users/queries.clj
@@ -5,12 +5,21 @@
   (:require
     [next.jdbc.sql :as query]))
 
-; inserir usuario
+; Inserir Usuário
 (defn inserir [id-profile name email password slug] (query/insert! ds :usuario
               {:idPerfil id-profile :Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.)}))
 
-;atualizar usuario pelo id
+;Atualizar Usuário pelo ID
 (defn atualizar [id name email password slug] (query/update! ds :usuario
-              {:Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.)}    {:idUsuario id } ))
+              {:Nome name :Email email :Senha password :Slug slug  :DataModficacao (java.util.Date.)} {:idUsuario id }))
 
+; Buscar Lista de Usuários
 (defn ler [request] (read-as-local) (query/query ds ["select * from usuario"]))
+
+; Buscar um Usuário pelo id
+(defn lerPorId [id]  (query/get-by-id ds :usuario  id :idUsuario {}))
+
+
+
+;Deletar Usuário pelo ID
+(defn deletar [id]  (query/delete! ds :usuario { :idUsuario id}))
